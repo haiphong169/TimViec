4747aaf603e65942a3f875f0dc069a9e52263bdd
Phong <bjergsen169@gmail.com>
2022-10-31 03:22:51 +0700

    intro_module

---
+++ app/src/main/java/com/example/timviec/intro/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.timviec.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timviec.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/example/timviec/intro/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.timviec.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timviec.R
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
