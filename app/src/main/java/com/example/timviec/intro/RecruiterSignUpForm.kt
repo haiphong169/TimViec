2417039af1706c4d047b25b43a8e18dac07c536a
Phong <bjergsen169@gmail.com>
2022-10-31 14:32:12 +0700

    update intro and signin screen

---
+++ app/src/main/java/com/example/timviec/intro/RecruiterSignUpForm.kt
@@ -0,0 +1,12 @@
+package com.example.timviec.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timviec.R
+
+class RecruiterSignUpForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recruiter_sign_up_form)
+    }
+}
