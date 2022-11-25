2d44a811343927ed8eb284b3a84f7aa02631b869
Hai Phong <bjergsen169@gmail.com>
2022-11-25 12:57:30 +0700

    profile module

---
+++ app/src/main/java/com/example/timviec/profile/EditRecruiterProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timviec.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditRecruiterProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_recruiter_profile)
+    }
+}
---
+++ app/src/main/java/com/example/timviec/profile/RecruiterProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timviec.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecruiterProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recruiter_profile)
+    }
+}
